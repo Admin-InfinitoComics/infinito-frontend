ed36432fea1ab7d61755c60b9edc51e7f01fcbf0
Admin-InfinitoComics <admin.it@infinitocomics.com>
2025-09-18 12:54:04 +0530

    Update Faqs.jsx

--- src/pages/Infinito Ultimate/Faqs.jsx
+++ src/pages/Infinito Ultimate/Faqs.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import FAQSection from '../../../../Research/src/pages/Home/FAQSection'
+
 
 const Faqs = () => {
   return (
     <div className='px-9 lg:px-[12.5rem] py-12 mt-16 mb-16 bg-gray-100'>
-      <FAQSection category="research" />
+
     </div>
   )
 }
