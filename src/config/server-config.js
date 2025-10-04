727d015ed6d85670df918b8df68157ec9a69b43c
Admin-InfinitoComics <admin.it@infinitocomics.com>
2025-10-04 19:06:32 +0530

    Update server-config.js

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -1,4 +1,4 @@
 // Server configuration
 
 // Export the backend URL for API requests
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4500";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://api.infinitocomics.com";
